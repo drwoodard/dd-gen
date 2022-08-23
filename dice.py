477b84cac2675bc5609b35ce1fbf06b03b9fe4ea
Dan Woodard <dwoodard@allegisgroup.com>
2022-08-23 05:32:27 -0400

    cleaned up comments

--- dice.py
+++ dice.py
@@ -3,12 +3,6 @@ from numbers import Number
 import random
 
 class Dice:
-    """
-        A static class to perform basic dice operations.
-    """
-    # def __init__(self, sides:int) -> None:
-    #     self.__sides = sides
-
     @staticmethod
     def roll( sides: int, times:int = 1) -> int: 
         """ Roll an n-sided di.
@@ -21,4 +15,6 @@ class Dice:
         for x in range(int(times)):
             outcome += random.randrange(1, sides)
 
+        # This code could use some better testing, I'm re-rolling one di in case of a zero. 
+        # This should never happen, but it did
         return outcome if outcome > 0 else Dice.roll(1)
